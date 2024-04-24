39c3854358d8f45e949e68c7f976cb64855dcfdb
Kajuksa <160936783+Kajuksa@users.noreply.github.com>
2024-04-24 09:58:21 +0300

    PEP8
    Modulių importavimo tvarka pakeista atitinkamai PEP 8 standartams.
    Klasės ir metodų pavadinimai pertvarkyti, kad atitiktų PEP 8 reikalavimus.
    Būtent metodų pavadinimai ir argumentai pertvarkyti, panaudojant "snake_case" stilių.
    Atidarymo ir uždarymo skliaustai pertvarkyti pagal PEP 8 reikalavimus.
    Trumpi komentarai pridėti prie kodo eilučių, kad paaiškintų jų funkcionalumą.
    Kitos smulkios stiliaus taisymo priemonės taikytos ten, kur buvo reikalinga jų pataisa, kad kodas atitiktų PEP 8.

--- cell.py
+++ cell.py
@@ -1,5 +1,5 @@
-from tkinter import Button, Label
 import tkinter as tk
+from tkinter import Button, Label
 import settings
 import random
 import ctypes
@@ -14,7 +14,7 @@ class Cell:
         data = file.readlines()
         wins, loses = map(int, (data[0].split(":")[1].strip(), data[1].split(":")[1].strip()))
 
-    def __init__(self, x, y, is_mine = False):
+    def __init__(self, x, y, is_mine=False):
         self.is_mine = is_mine
         self.is_opened = False
         self.is_mine_candidate = False
@@ -22,27 +22,27 @@ class Cell:
         self.x = x
         self.y = y
 
-        #Prideti objiekta i lista
+        # Pridėti objektą į sąrašą
         Cell.all.append(self)
 
-    def create_btn_object(self, location): 
-        btn = Button (
+    def create_btn_object(self, location):
+        btn = Button(
             location,
-            width = 12,
-            height = 4
+            width=12,
+            height=4
         )
         btn.bind('<Button-1>', self.left_click)
         btn.bind('<Button-3>', self.right_click)
         self.cell_btn_object = btn
 
-    @staticmethod #naudoti ne kiekvienam cellui o visai klasei viena karta
+    @staticmethod  # naudoti ne kiekvienam cellui o visai klasei vieną kartą
     def create_cell_count_label(location):
         label = Label(
             location,
-            bg = 'black',
-            fg = 'white',
-            text = f"Cells Left:{Cell.cell_count}",
-            font = ("", 35)
+            bg='black',
+            fg='white',
+            text=f"Cells Left:{Cell.cell_count}",
+            font=("", 35)
         )
         Cell.cell_count_label_object = label
 
@@ -53,43 +53,41 @@ class Cell:
             Cell.loses += 1
             Cell.write_game_info()
         else:
-            if self.surrounding_cells_mines_lenght == 0:
+            if self.surrounding_cells_mines_length == 0:
                 for cell_obj in self.surrounding:
-                   cell_obj.show_cell() 
+                    cell_obj.show_cell()
             self.show_cell()
-        #Laimejimas
+        # Laimėjimas
         if Cell.cell_count == settings.MINES:
             ctypes.windll.user32.MessageBoxW(0, 'Congratulations! You won the game!', 'Win, win, win!', 0)
             Cell.wins += 1
             Cell.write_game_info()
             self.reset_game()
-        #cancel left click if cell is opened
+        # cancel left click if cell is opened
         self.cell_btn_object.unbind('<button-1')
 
     def get_cell_by_axis(self, x, y):
-        # return a cell object based on the value of x and y
         for cell in Cell.all:
             if cell.x == x and cell.y == y:
                 return cell
 
-
-    @property #The @property is a built-in decorator for the property() function in Python. It is used to give "special" functionality to certain methods to make them act as getters, setters, or deleters when we define properties in a class
+    @property  # The @property is a built-in decorator for the property() function in Python. It is used to give "special" functionality to certain methods to make them act as getters, setters, or deleters when we define properties in a class
     def surrounding(self):
         cells = [
-            self.get_cell_by_axis(self.x-1, self.y-1),
-            self.get_cell_by_axis(self.x-1, self.y),
-            self.get_cell_by_axis(self.x-1, self.y+1),
-            self.get_cell_by_axis(self.x, self.y-1),
-            self.get_cell_by_axis(self.x, self.y+1),
-            self.get_cell_by_axis(self.x+1, self.y-1),
-            self.get_cell_by_axis(self.x+1, self.y),
-            self.get_cell_by_axis(self.x+1, self.y+1)
+            self.get_cell_by_axis(self.x - 1, self.y - 1),
+            self.get_cell_by_axis(self.x - 1, self.y),
+            self.get_cell_by_axis(self.x - 1, self.y + 1),
+            self.get_cell_by_axis(self.x, self.y - 1),
+            self.get_cell_by_axis(self.x, self.y + 1),
+            self.get_cell_by_axis(self.x + 1, self.y - 1),
+            self.get_cell_by_axis(self.x + 1, self.y),
+            self.get_cell_by_axis(self.x + 1, self.y + 1)
         ]
         cells = [cell for cell in cells if cell is not None]
         return cells
-    
+
     @property
-    def surrounding_cells_mines_lenght(self):
+    def surrounding_cells_mines_length(self):
         counter = 0
         for cell in self.surrounding:
             if cell.is_mine:
@@ -100,18 +98,17 @@ class Cell:
     def show_cell(self):
         if not self.is_opened:
             Cell.cell_count -= 1
-            #print(self.surrounding_cells_mines_lenght) #tikrinu ar veikia surroundinf_cells_mines_lenght
-            self.cell_btn_object.configure(text = self.surrounding_cells_mines_lenght)
-            #Skaiciuoja kiek liko ejimu
+            # print(self.surrounding_cells_mines_length) # tikrinu ar veikia surroundinf_cells_mines_lenght
+            self.cell_btn_object.configure(text=self.surrounding_cells_mines_length)
+            # Skaiciuoja kiek liko ejimu
             if Cell.cell_count_label_object:
                 Cell.cell_count_label_object.configure(
-                    text = f"Cells Left:{Cell.cell_count}"
-                    )
-            #Jei pasirinkau kaip mine_candidate bet nusprendziau paspaust kad convertintu i systemos spalva
-            self.cell_btn_object.configure(bg = 'SystemButtonFace')
-        #Pazymiu kad langelis yra atidarytas
+                    text=f"Cells Left:{Cell.cell_count}"
+                )
+            # Jei pasirinkau kaip mine_candidate bet nusprendziau paspaust kad convertintu i systemos spalva
+            self.cell_btn_object.configure(bg='SystemButtonFace')
+        # Pazymiu kad langelis yra atidarytas
         self.is_opened = True
-        
 
     def show_mine(self):
         self.cell_btn_object.configure(bg='red')
@@ -129,9 +126,9 @@ class Cell:
     @staticmethod
     def random_mines():
         picked_cells = random.sample(
-            Cell.all, 
+            Cell.all,
             settings.MINES
-        ) #sample yra metodas kuris paziuri pirma i lista ir td kiek bombu mum reik
+        )  # sample yra metodas kuris paziuri pirma i lista ir td kiek bombu mum reik
         for picked_cell in picked_cells:
             picked_cell.is_mine = True
 
@@ -161,7 +158,8 @@ class Cell:
     @staticmethod
     def write_game_info():
         filename = "game_info.txt"
-        with open(filename, "w") as file:  # Open in write mode to overwrite
-            record = f"Wins: {Cell.wins}\nLoses: {Cell.loses}"  # Update record
+        with open(filename, "w") as file: 
+            record = f"Wins: {Cell.wins}\nLoses: {Cell.loses}" 
+            file.write(record)
             file.write(record)
 
